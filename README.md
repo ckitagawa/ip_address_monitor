ff69e38db309f57c0937aeadc1b23d6ff1dbc5fd
ckitagawa <calderk2010@gmail.com>
2019-05-08 14:24:39 -0400

    Working script

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# ip_addresss_monitor
-Simple External IP Address Monitoring Solution
+# IP Address Monitor
+
+A simple external IP address monitoring solution that sends an email when the
+external IP address is changed and a configurable reminder that is sent
+periodically if the IP hasn't changed.
+
+## Usage
+
+Clone the repository and fill in the fields in the `conf.json` file as
+outlined. The `usr` and `pwd` fields are for logging into the SMTP server.
+Ideally, the `pwd` field or the entire `conf.json` would be encrypted...
+
+If using Gmail I recommend creating a new dev account. You need to turn on
+access for Less Secure Apps or if using 2FA get a token. The latter case
+requires rewriting some of the SMTP code. If not using Gmail the SMTP domain in
+the `ip_addr_mon.py` script needs to be updated.
+
+Copy the following bash script and fill out the fields.
+
+```bash
+#!/usr/bin/sh
+
+cd <directory>
+/usr/bin/python <directory>/ip_addr_mon.py
+```
+
+Schedule a cron job to run at the desired monitoring frequency that runs the
+aforementioned script. Use Crontab on Linux for example.
+
+Good devices for this to run on are a server, BeagleBone or RaspberryPi.
+
+## Motivation
+
+It isn't always possible to get a static external IP as a non-business
+customer. Some IOT, home entertainment servers and home security devices
+require you to address/access them via IP. If away from home, checking your
+external IP can be a hassle/challenge. This monitoring solution allows for
+periodic checks to your external IP when away from home and alerting via email,
+which is simple enough that anyone can use it.
+
+## Requirements
+
+Python 3, and the requests package (sometimes not in the default installation).
+
+In theory this should work on any device that runs Python (Linux, Windows,
+MacOS). Although I've only tested it on low power Linux devices.
+
---
+++ ip_addr_mon.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 ckitagawa
+#
+# Distributed under terms of the MIT license.
+
+import email
+import json
+import logging
+import os
+import requests
+import smtplib
+import time
+
+NO_OP = 0
+INFO = 1
+ALERT = 2
+
+REMINDER_THRESHOLD_SECS = (6 * 24 + 23) * 3600
+
+
+def GetExternalIp():
+    """Gets External IP from web API."""
+    return requests.get('https://api.ipify.org').text
+
+
+def CheckAndUpdateCache(ip, filename='cache.txt'):
+    """Checks cache file and updates. Returns the action to take.
+
+    - Alerts if IP is changed since last run.
+    - Sends reminders every REMINDER_THRESHOLD_SECS if IP is unchanged.
+
+    Args:
+      ip: the current IP address as a str.
+      filename: the filename of the cache file.
+
+    Returns:
+      kind of action to take {NO_OP, INFO, ALERT}
+    """
+    if os.path.exists(filename):
+        with open(filename) as f:
+            old_ip = f.readline()
+        if old_ip.rstrip() == ip:
+            ot = os.path.getmtime(filename)
+            t = time.time()
+            if (t - ot) >= REMINDER_THRESHOLD_SECS:
+                return INFO
+            else:
+                return NO_OP
+    with open(filename, 'w') as f:
+        f.write('{}\n'.format(ip))
+    return ALERT
+
+
+def SendMessage(msg, usr, pwd, domain='smtp.gmail.com', port=587):
+    """Sends an email message via SMTP.
+
+    Args:
+      msg: an EmailMessage object with To, From, Subject and content filled in.
+      usr: the username for the SMTP service.
+      pwd: the password for the SMTP service.
+      domain: the SMTP domain (default is GMail).
+      port: the SMTP port.
+
+    Returns:
+      Nothing, but will log any exceptions that occur.
+    """
+    try:
+        server = smtplib.SMTP(domain, port)
+        server.ehlo()
+        server.starttls()
+        server.login(usr, pwd)
+        server.send_message(msg)
+    except Exception as e:
+        logging.exception('Error in SendMessage: {}'.format(e))
+
+
+def LoadConfig(filename='conf.json'):
+    """Loads configuration file (filename arg)."""
+    with open(filename) as f:
+        return json.loads(f.read())
+
+
+def CreateEmail(data, ip, kind):
+    """Creates an email with IP address information.
+
+    Args:
+      data: a dictionary like object with 'From' and 'To' keys.
+      ip: the ip address to send as a str.
+      kind: one of {INFO, ALERT} to specify email subject line.
+
+    Returns:
+      An EmailMessage object.
+    """
+    msg = email.message.EmailMessage()
+    msg['From'] = data['From']
+    msg['To'] = data['To']
+    msg.set_content('Currently, the IP address is: {}'.format(ip))
+
+    if kind == INFO:
+        msg['Subject'] = 'Info: External IP Address Reminder'
+    elif kind == ALERT:
+        msg['Subject'] = 'Alert: External IP Address Changed'
+
+    return msg
+
+
+if __name__ == '__main__':
+    ip = GetExternalIp()
+    kind = CheckAndUpdateCache(ip)
+    if kind != NO_OP:
+        data = LoadConfig()
+        msg = CreateEmail(data, ip, kind)
+        SendMessage(msg, data['usr'], data['pwd'])
